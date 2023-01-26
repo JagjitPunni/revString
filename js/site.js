4a13aa561057986131d97e56821fc4faf72a16e5
Jagjit Punni <j.punni@gmx.de>
2023-01-26 01:45:50 +0100

    Update site.js
    Reverse user input string and display on HTML page using functions, DOM, and loops.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,36 @@
-//Call Hello World
-function helloWorld(){
-    alert('Hello World');
+//Get the values (string) from the page
+//Controller function
+function getValue(){
+    document.getElementById('alert').classList.add('invisible');
+    let userString = document.getElementById('userString').value;
+    let revString = reverseString(userString);
+    displayString(revString,userString);
+}
+
+
+//Reverse the string
+//Logic function
+function reverseString(userString){
+
+    let revString = [];
+
+    
+    //reverse a string using a for loop
+    for (let i = userString.length - 1; i >= 0; i--) {
+        revString += userString[i];
+        
+    }
+    return revString;
+}
+
+
+//Display the reversed string to the user
+//View function
+function displayString(revString,userString){
+
+    //write to the page
+    document.getElementById('msg').innerHTML = `${revString} is the reverse of ${userString}.`;
+    //show the alert box
+    document.getElementById('alert').classList.remove('invisible');
+    
 }
